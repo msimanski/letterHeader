dff6e0ea9edb4126245809f56433cd42bac11f22
Michael Simanski <mfsimanski@gmail.com>
2019-07-08 19:37:03 -0400

    Expanded framework

--- src/letterHeader/CreateDocument.java
+++ src/letterHeader/CreateDocument.java
@@ -2,10 +2,14 @@ package letterHeader;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTPageBreak;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPageMar;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
 
 public class CreateDocument 
 {
@@ -20,7 +24,7 @@ public class CreateDocument
 			
 	// create runner objects
 	static XWPFRun nameRunner = name.createRun();
-	static XWPFRun addressRunner = name.createRun();
+	static XWPFRun addressRunner = address.createRun();
 	// remember to make a runner for each email
 	static ArrayList<XWPFRun> phoneRunners = new ArrayList<XWPFRun>();
 	static ArrayList<XWPFRun> emailRunners = new ArrayList<XWPFRun>();
@@ -30,27 +34,61 @@ public class CreateDocument
 		// create IO stream with document name
 		FileOutputStream out = new FileOutputStream( new File("createdocument.docx"));
 		
+		CTSectPr sectPr = document.getDocument().getBody().addNewSectPr();
+		CTPageMar pageMar = sectPr.addNewPgMar();
+		pageMar.setLeft(BigInteger.valueOf(720L));
+		pageMar.setTop(BigInteger.valueOf(720L));
+		pageMar.setRight(BigInteger.valueOf(720L));
+		pageMar.setBottom(BigInteger.valueOf(720L));
+		
+		// storing the ID automatically makes the objects
+		int phoneID = addListParagraph("phone");
+		int emailID = addListParagraph("email");
+		
+		// make name
+		nameRunner.setText("Michael Simanski \n");
+		nameRunner.setBold(true);
+		nameRunner.setFontSize(18);
+		nameRunner.setFontFamily("Times");
+		
+		// make address
+		addressRunner.setText("2907 S 10th Ave, Altoona, PA 16601 \n");
+		nameRunner.setFontSize(12);
+		nameRunner.setFontFamily("Times");
+		
+		// make phone
+		phoneRunners.get(phoneID).setText("(814) 414 - 9770");
+		phoneRunners.get(phoneID).setFontSize(12);
+		phoneRunners.get(phoneID).setFontFamily("Times");
+		
+		// make email
+		emailRunners.get(emailID).setText("mfsimanski@gmail.com");
+		emailRunners.get(emailID).setFontSize(12);
+		emailRunners.get(emailID).setFontFamily("Times");
+		
 		document.write(out);
 		out.close();
 		
 
 	}
 	
-	public static void addListParagraph(String type, String text) 
+	public static int addListParagraph(String type) 
 	{
 		switch (type) 
 		{
 			case "phone":
 				phoneNumbers.add(document.createParagraph());
-				phoneRunners.add(emails.get(emails.size() - 1).createRun());
-				break;
+				phoneRunners.add(phoneNumbers.get(phoneNumbers.size() - 1).createRun());
+				return phoneNumbers.size() - 1;
 				
 			case "email":
-				
-				break;
+				emails.add(document.createParagraph());
+				emailRunners.add(emails.get(emails.size() - 1).createRun());
+				return emails.size() - 1;
 				
 			default:
-				
+				System.out.println("ERROR: Paragraph type not found!");
+				return 0;
 		}
 	}
 
